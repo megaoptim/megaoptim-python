c29de07c8b9b800feaba0fe309e8a4a9029164fa
dg@darkog.com <dg@darkog.com>
2020-05-23 00:08:47 +0200

    Added compatibility for python 2.7 in build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
-version=$1
+build_version=$1
 
-sudo pip3 uninstall megaoptim
-rm -rf dist/megaoptim-$version.tar.gz
-python3 setup.py sdist
-sudo pip3 install dist/megaoptim-$version.tar.gz
+if [ -z "$build_version" ]
+  then
+    echo "No version provided"
+    exit
+fi
+
+if [[ "$(python3 -V)" =~ "Python 3" ]]
+then
+  bin_pip="pip3"
+  bin_python="python3"
+else
+  bin_pip="pip"
+  bin_python="python"
+fi
+
+sudo "$bin_pip" uninstall megaoptim
+rm -rf dist/megaoptim-$build_version.tar.gz
+"$bin_python" setup.py sdist
+sudo "$bin_pip" install dist/megaoptim-$build_version.tar.gz
